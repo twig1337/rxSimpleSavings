f23c783e6bcd28c8f2acae1bd90a903dde10a0e9
Thomas Ellsworth <tellsworth@rxsavingssolutions.com>
2019-04-24 20:48:43 -0500

    Added local notifications.

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,8 +1,23 @@
 import { Component } from '@angular/core';
+import { LocalNotifications } from '@ionic-native/local-notifications/ngx';
+
 
 @Component({
   selector: 'app-tab3',
   templateUrl: 'tab3.page.html',
   styleUrls: ['tab3.page.scss']
 })
-export class Tab3Page {}
+export class Tab3Page {
+  constructor(private localNotifications: LocalNotifications) {}
+
+  pusher() {
+    // Schedule a single notification
+    this.localNotifications.schedule({
+      id: 1,
+      text: 'I HAVE BECOME DEATH! DESTROYER OF WORLDS!',
+      foreground: true,
+      data: { secret: 'MOOHAHAHHAHAHAHAHA' }
+    });
+
+  }
+}
