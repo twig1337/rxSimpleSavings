fffa23954b276654ce240109438d2ab5a9361c96
rxnnolan <nnolan@rxsavingssolutions.com>
2019-04-24 19:01:19 -0500

    Adding tab2

---
+++ src/data/data.ts
@@ -0,0 +1,9 @@
+export interface data {
+    Type: string;
+    Cost: number;
+}
+
+export const StatsBarChart: data[] = [
+    {Type: 'Generic', Cost: 10},
+    {Type: 'Name Brand', Cost: 1000}
+];
